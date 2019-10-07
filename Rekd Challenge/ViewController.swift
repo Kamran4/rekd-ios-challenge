0a665d5f06cadcde10ae423454fe3a9547954c39
shahyan <shahyan@rekd-app.com>
2019-10-07 13:37:47 -0400

    Initial Commit

---
+++ Rekd Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rekd Challenge
+//
+//  Created by Rekd TechOps on 10/7/19.
+//  Copyright © 2019 Rekd Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
